28c435e3cffe334b776d3f75a19e42acc01b9204
kainyzrwaw <sydney.wawrzyniak@kellogg.northwestern.edu>
2022-04-14 01:20:17 +0000

    Changes

--- school/school.sql
+++ school/school.sql
@@ -14,6 +14,134 @@ CREATE TABLE students (
   phone_number TEXT
 );
 
--- Create the rest of the tables
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
 
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
 
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
+
+INSERT INTO students (
+    first_name,
+    last_name,
+    email,
+    phone_number
+) VALUES (
+    "Jane",
+    "Doe",
+    "jane@example.com",
+    "555-1212"
+);
+
+INSERT INTO students (
+    first_name,
+    last_name,
+    email,
+    phone_number
+) VALUES (
+    "Jenny",
+    "Smith",
+    "jenny@gmail.com",
+    "867-5309"
+);
+
+INSERT INTO students (
+    first_name,
+    last_name,
+    email,
+    phone_number
+) VALUES (
+    "John",
+    "Johnson",
+    "john@acme.com",
+    "456-7890"
+);
+
+INSERT INTO teachers (
+    first_name,
+    last_name,
+    bio
+) VALUES (
+    "Ben",
+    "Block",
+    "A seasoned pro instructor."
+);
+
+INSERT INTO teachers (
+    first_name,
+    last_name,
+    bio
+) VALUES (
+    "Brian",
+    "Eng",
+    "Loves tacos."
+);
+
+INSERT INTO courses (
+    name,
+    description
+) VALUES (
+    "Intro to Software Development",
+    "This course is focused on software development..."
+);
+
+INSERT INTO courses (
+    name,
+    description
+) VALUES (
+    "Taco Making 101",
+    "In this course, you'll learn how to build a proper taco..."
+);
+
+INSERT INTO sections (
+    time,
+    course_id,
+    teacher_id
+) VALUES (
+    "Tuesday 8:30-11:30",
+    "1",
+    "2"
+);
+
+INSERT INTO sections (
+    time,
+    course_id,
+    teacher_id
+) VALUES (
+    "Wednesday 6-9pm",
+    "1",
+    "1"
+);
+
+INSERT INTO sections (
+    time,
+    course_id,
+    teacher_id
+) VALUES (
+    "Wednesday 6-9pm",
+    "2",
+    "2"
+);
+
+INSERT INTO sections (
+    time,
+    course_id,
+    teacher_id
+) VALUES (
+    "Thursday 6-9pm",
+    "2",
+    "1"
+);
